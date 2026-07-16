bc5c34b7fe8f23edbc1530012346033204a1b35d
sunyis <572383342@qq.com>
2026-07-16 10:15:04 +0800

    Add files via upload

--- core/ip_detector.py
+++ core/ip_detector.py
@@ -3,15 +3,21 @@ import json
 import socket
 from typing import Optional
 
-
 class IPDetector:
     """公网 IP 检测器"""
 
+    # 使用 2 个稳定的 API，防止一个失效自动尝试第二个
     DETECT_URLS = [
         "https://api.ipify.org?format=json",
         "https://ifconfig.me/all.json",
-        "https://api.myip.com",
+        "http://checkip.spdyn.de",
+        "http://checkip.feste-ip.net",
+        "https://api.ip.sb/geoip",
         "http://ip-api.com/json",
+        "http://ipv4.wzzw.eu.cc/ip.php?json=1",
+        "http://ip.wzzw.eu.cc/myip.php?json=1",
+        "https://api.mir6.com/api/ip_json",
+        "https://api-ipv6.ip.sb/geoip",
     ]
 
     @staticmethod
@@ -24,14 +30,14 @@ class IPDetector:
                 })
                 with urllib.request.urlopen(req, timeout=5) as resp:
                     data = json.loads(resp.read().decode())
-                    # 不同 API 返回字段不同
-                    for key in ("ip", "query", "origin"):
+                    # 不同 API 返回字段不同 (ipify 返回 ip, ip-api 返回 query)
+                    for key in ("ip", "query", "ip_addr", "Current IP Address", "origin"):
                         if key in data:
                             return data[key]
             except Exception:
                 continue
 
-        # 备用：直接通过 socket 连接外部获取
+        # 备用：如果公网 API 都失败，尝试获取局域网 IP
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             s.connect(("8.8.8.8", 80))
